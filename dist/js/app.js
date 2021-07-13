4a9efd56c9f0c4e66920536aacbfedc2dcf8a720
Reff20 <fernandoperes.ctt@gmail.com>
2021-07-13 14:51:36 -0300

    Basic table in typescript

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    controller.adiciona();
+});
